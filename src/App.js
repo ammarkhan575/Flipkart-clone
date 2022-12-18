42b88d5f2e9428bdfce76611fe8b1d030cebbd6a
ammar <ammarkhan575@gmail.com>
2022-12-19 01:37:31 +0530

    categories added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+
 
 // components
 import Header from './components/header/Header';
+import Home from './components/home/Home'
 
+import {Box} from '@mui/material'
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Box style={{marginTop: 54}}>
+        <Home/>
+      </Box>
+      
+    </>
   );
 }
 
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+
+import NavBar from './NavaBar';
+import {Box} from '@mui/material'
+
+const Home = ()=>{
+    return (
+        <Box>
+            <NavBar/>
+        </Box>
+    )
+}
+
+export default Home;
