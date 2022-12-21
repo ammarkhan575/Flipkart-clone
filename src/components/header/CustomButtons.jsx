5e62e71ec9efa1e457ef027bb46afc125e3e30bf
ammar <ammarkhan575@gmail.com>
2022-12-21 13:59:19 +0530

    signup functionality completed

--- src/components/header/CustomButtons.jsx
+++ src/components/header/CustomButtons.jsx
@@ -1,6 +1,9 @@
-
+import { useState } from 'react'
 import {Box,Button, Typography,styled} from '@mui/material'
 import {ShoppingCart} from '@mui/icons-material'
+
+// components
+import LoginDialog from '../login/LoginDialog'
 
 const Wrapper = styled(Box)`
     display: flex;
@@ -27,9 +30,13 @@ const LoginButton = styled(Button)`
 `
 
 const CustomButtons = ()=>{
+    const [open, setOpen] = useState(false);
+    const openDialog = ()=>{
+        setOpen(true);
+    }
     return (
         <Wrapper>
-            <LoginButton variant='contained'>Login</LoginButton>
+            <LoginButton variant='contained' onClick={()=> openDialog()}>Login</LoginButton>
 
             <Typography style={{width: 135}}>Become a Seller</Typography>
             <Typography>More</Typography>
@@ -38,6 +45,7 @@ const CustomButtons = ()=>{
                 <ShoppingCart/>
                 <Typography>Cart</Typography>
             </FlexContainer>
+            <LoginDialog open={open} setOpen={setOpen}/>
         </Wrapper>
     )
 };
---
+++ src/components/login/LoginDialog.jsx
@@ -0,0 +1,155 @@
+import {useState} from 'react'
+import { Box, Button, Dialog, TextField, Typography,styled } from "@mui/material";
+
+import { authenticateSignup } from '../../service/api';
+
+const Component = styled(Box)`
+    height: 70vh;
+    width: 90vh;
+    display:flex;
+`
+
+const LeftBox = styled(Box)`
+    background: #2874f0 url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png) center 85% no-repeat;
+    height: 83.8%;
+    width: 30%;
+    padding: 45px 35px;
+    &>p, &>h5{
+        color: #fff;
+    }
+`;
+
+const RightBox = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    padding: 25px 35px;
+    flex: 1;
+    &>div, &>button,&>p{
+        margin-top: 20px;
+    }
+`;
+
+const LoginButton = styled(Button)`
+    text-transform: none;
+    background: #fb641b;
+    color: #fff;
+    height: 48px;
+    border-radius: 2px;
+`;
+
+const RequestOTP = styled(Button)`
+    text-transform: none;
+    background: #fff;
+    color: #2874f0;
+    height: 48px;
+    border-radius: 2px;
+    box-shadow: 0 2px 
+`;
+
+const Text = styled(Typography)`
+    font-size: 12px;
+    color: #878787;
+`
+
+const SignupText = styled(Typography)`
+    font-size: 14px;
+    color: #2874f0;
+    text-align: center;
+    font-weight: 600;
+    cursor: pointer;
+`
+
+const accountInitialValues ={
+    login: {
+        view: 'login',
+        heading: 'Login',
+        subHeading: 'Get access to your Orders, Wishlist and Recommendations'
+
+    },
+    signup: {
+        view: 'signup',
+        heading: "Looks like you're new here",
+        subHeading: "Sign up with your mobile number to get started"
+    }
+};
+
+const signupInitialValues = {
+    firstname: '',
+    lastname: '',
+    username: '',
+    email: '',
+    password: '',
+    phone: ''
+}
+
+const LoginDialog = ({open,setOpen})=>{
+
+    const [account, toggleAccount] = useState(accountInitialValues.login);
+    // change the state to signup
+    const toggleSignup = ()=>{
+        toggleAccount(accountInitialValues.signup);
+    }
+
+    const [signup, setSignup] = useState(signupInitialValues);
+
+
+    // to hide dialog box
+    const handleClose = ()=>{
+        setOpen(false);
+        toggleAccount(accountInitialValues.login);
+    }
+    // handle input change
+    const onInputChange = (e)=>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setSignup({...signup,[name]:value});
+    }
+
+    const signupUser = async ()=>{
+        // signup data passes, to send at backend
+        let response = await authenticateSignup(signup);
+        console.log(response);
+        if(!response){
+            return;
+        }
+        handleClose();
+    }
+
+    return(
+        <Dialog open={open} onClose={handleClose} PaperProps={{sx: {maxWidth: 'unset'}}}>
+        {/* Login dialog box container */}
+            <Component>
+                {/* left side */} 
+                <LeftBox>
+                    <Typography variant="h5">{account.heading}</Typography>
+                    <Typography style={{marginTop: 20}}>{account.subHeading}</Typography>
+                </LeftBox>
+                {/* right side */}
+                {
+                    account.view==='login' ?
+                    <RightBox>
+                        <TextField variant="standard" label='Enter Email/Mobile Number'/>
+                        <TextField variant="standard" label='Enter password'/>
+                        <Text>By continuing, you agree to Flipkart's Terms of Use and Privacy Policy</Text>
+                        <LoginButton>Login</LoginButton>
+                        <Typography style={{textAlign: 'center'}}>OR</Typography>
+                        <RequestOTP>Request OTP</RequestOTP>
+                        <SignupText onClick={toggleSignup}>New to Flipkart? Create an account</SignupText>
+                    </RightBox>
+                    :
+                    <RightBox>
+                        <TextField variant="standard" onChange={(e)=> onInputChange(e)} label='Enter First Name' name='firstname'/>
+                        <TextField variant="standard" onChange={(e)=> onInputChange(e)} label='Enter Last Name' name='lastname'/>
+                        <TextField variant="standard" onChange={(e)=> onInputChange(e)} label='Enter Username' name='username'/>
+                        <TextField variant="standard" onChange={(e)=> onInputChange(e)} label='Enter Email' name='email'/>
+                        <TextField variant="standard" onChange={(e)=> onInputChange(e)} label='Enter Password' name='password'/>
+                        <TextField variant="standard" onChange={(e)=> onInputChange(e)} label='Enter Phone' name='phone'/>
+                        <LoginButton onClick={()=>signupUser()}>Continue</LoginButton>
+                    </RightBox>
+                }
+            </Component>
+        </Dialog>
+    )
+}
+
+export default LoginDialog;
